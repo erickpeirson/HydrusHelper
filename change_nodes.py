7bfd1c4bcec6e3f61762a60d8ab4e26ff15da09f
Erick Peirson <erick.peirson@asu.edu>
2016-03-09 11:25:44 -0500

    cleanup

--- change_nodes.py
+++ change_nodes.py
@@ -1,5 +1,7 @@
-# Usage:
-#  python change_nodes.py [path to workspace]
+"""
+Usage:
+  python change_nodes.py [path to workspace]
+"""
 
 import sys
 import os
@@ -17,7 +19,7 @@ with open(hpath, 'rU') as f:
 
 profile[-1] = '   '.join(['']+ [str(i) for i in range(1,62)])
 profile[-2] = '   61'
-    
+
 with open(hpath, 'w') as f:
     print "writing", hpath, "...",
     f.write('\n'.join(profile))
--- process_workspace.py
+++ process_workspace.py
@@ -1,5 +1,10 @@
-# Usage:
-#  $ python process_workspace [path to workspace] [path to output directory]
+"""
+Usage:
+
+```shell
+$ python process_workspace [path to workspace] [path to output directory]
+```
+"""
 
 import sys
 import os
@@ -20,32 +25,31 @@ if not os.path.exists(h_dir):
 if not os.path.exists(theta_dir):
     os.makedirs(theta_dir)
 
-
 # Ignore any file or directory that does not contain an 'Obs_Node.out' file.
 obs_path = os.path.join(md, 'Obs_Node.out')
 if os.path.exists(obs_path):
     head, d = os.path.split(md)
     print d,"::",
-    
+
     # Output files are named by the Hydrus output directory name and parameter.
     h_path = os.path.join(h_dir, f_format.format(dataset=d, param='h'))
-    theta_path = os.path.join(theta_dir, f_format.format(dataset=d, param='theta'))        
+    theta_path = os.path.join(theta_dir, f_format.format(dataset=d, param='theta'))
 
-    # Skip any directories for which an h data file already exists.        
+    # Skip any directories for which an h data file already exists.
     if not os.path.exists(h_path):
         print 'processing...',
         with open(obs_path, 'r') as f:
-            raw = f.readlines()            
-        
+            raw = f.readlines()
+
         data = []
         for line in raw[10:-1]:    # Discard Hydrus headers and "end" line.
             # The Hydrus data file is white-space delimited.
             data.append(re.split('\s+', line))
-            
+
         columns = data[0][0:2] + data[0][2:5]*61 + [data[0][-1]]
         df = pd.DataFrame(data[1:], columns=columns)
-        df.h.to_csv(h_path, header=False, index=False)    
-        df.theta.to_csv(theta_path, header=False, index=False)                
+        df.h.to_csv(h_path, header=False, index=False)
+        df.theta.to_csv(theta_path, header=False, index=False)
         print 'done'
     else:
         print 'already processed or name conflict, skipping'
